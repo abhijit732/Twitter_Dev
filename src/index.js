0c5d20ec0862b44c8b66a8a71c01ea411ca966d3
Abhijit <abhijeetpurvey234@gmail.com>
2024-06-05 13:31:23 +0530

    Added Tweet Service with Hashtag Processing

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ const express= require('express');
 const connect = require('./config/database');
 const app=express();
 
-const Tweet=require('./models/tweet');
+
 app.listen(3000, async ()=>{
     console.log('Server started at Port 3000');
     await connect();
     console.log('Mongo DB Connected');
-    
-    const tweets= await Tweet.find({
-        content:["First Tweet", "Third tweet", "12345"]
-    });
-
-    console.log(tweets);
-
-    
 }); 
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,22 +1,29 @@
-const TweetRepository=require('./tweet-repository/index');
+const {TweetRepository, HashtagRepository}=require('../repository/index');
 
 class TweetService {
     constructor() {
-        this.tweetRepository=new TweetRepository;
+        this.tweetRepository=new TweetRepository();
+        this.hashtagRepository= new HashtagRepository();
     }
 
     async create(data) {
         const content=data.content;
-        const tags=content.match(/#[a-z0-9_]+/g); //this regex extracts hashtags
-        tags=tags.map((tag)=> tag.substring(1));
-        console.log(tags);
+        const tags=content.match(/#[a-zA-Z0-9_]+/g).map((tag)=> tag.substring(1));
         const tweet=await this.tweetRepository.create(data);
-        //todo create hashtags and add here
-        /** 
-         * 1. Bulkcreate in mongoose
-         * 2. Filter title of hashtag based on multiple tags
-         * 3. How to add tweet id inside all the hashtags
-         */
+        let alreadyPresentTags= await this.hashtagRepository.findByName(tags);
+        let titleOfPresentTags=alreadyPresentTags.map(tags=> tags.title);
+        let newTags=tags.filter(tag=> !titleOfPresentTags.includes(tag));
+        newTags=newTags.map(tag=>{
+            return {
+                title:tag,
+                tweets:[tweet.id]
+            }
+        });
+        await this.hashtagRepository.bulkCreate(newTags);
+        alreadyPresentTags.forEach((tag)=>{
+            tag.tweets.push(tweet.id);
+            tag.save();
+        })
         return tweet;
     }
 }
