44de6c63d4e8d5e95390f0d68036604344114fa0
Abhijit <abhijeetpurvey234@gmail.com>
2024-06-05 16:46:35 +0530

    Moved to ES6 modules

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-const express= require('express');
-const connect = require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
 const app=express();
 
-
+import service from './services/tweet-service.js'
 app.listen(3000, async ()=>{
     console.log('Server started at Port 3000');
     await connect();
     console.log('Mongo DB Connected');
+
+    let ser= new service();
+    await ser.create({content:'Done with #refactor?'});
 }); 
