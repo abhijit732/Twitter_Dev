abeffc8fe3ce453be1a1d4d4b20302ea3dfc800a
Abhijit <abhijeetpurvey234@gmail.com>
2024-06-20 13:03:37 +0530

    Toggle Likes Implemented

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import {connect} from './config/database.js';
 
 import apiRoutes from './routes/index.js';
 
+import {UserRepository, TweetRepository} from './repository/index.js';
+import LikeService from './services/like-service.js';
+
 const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -14,4 +17,17 @@ app.listen(3000, async ()=>{
     console.log('Server started at Port 3000');
     await connect();
     console.log('Mongo DB Connected');
+
+    const userRepo=new UserRepository();
+    const tweetRepo=new TweetRepository();
+
+    const tweets=await tweetRepo.getAll(0,10);
+
+
+    const users=await userRepo.getAll();
+
+    const likeService=new LikeService();
+    await likeService.toggleLike(tweets[0].id,'Tweet',users[0].id);
+
+
 }); 
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -22,6 +22,15 @@ class TweetRepository extends CrudRepository {
             console.log(error);
         }
     }
+
+    async find(id) {
+        try {
+            const tweet=await Tweet.findById(id).populate({path:'likes'});
+            return tweet;
+        } catch (error) {
+            
+        }
+    }
 }
 
 export default TweetRepository;
