17ac43516f8d83ac2c6820e29b59f62d8a360a82
Abhijit <abhijeetpurvey234@gmail.com>
2024-07-02 14:35:21 +0530

    Added passport logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 import {connect} from './config/database.js';
+import { passportAuth } from './config/jwt-middleware.js'; 
+
 
 import apiRoutes from './routes/index.js';
 
@@ -11,23 +14,15 @@ const app=express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(passport.initialize());
+passportAuth(passport);
+
 app.use('/api',apiRoutes);
 
 app.listen(3000, async ()=>{
     console.log('Server started at Port 3000');
     await connect();
     console.log('Mongo DB Connected');
-
-    // const userRepo=new UserRepository();
-    // const tweetRepo=new TweetRepository();
-
-    // const tweets=await tweetRepo.getAll(0,10);
-
-
-    // const users=await userRepo.getAll();
-
-    // const likeService=new LikeService();
-    // await likeService.toggleLike(tweets[0].id,'Tweet',users[0].id);
 
 
 }); 
